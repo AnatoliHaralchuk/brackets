9bf57e0bd3775f1691da4d0e6ec3da2bb018671f
AnatoliHaralchuk <sshades@mail.ru>
2021-03-12 23:32:31 +0300

    fix:test4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-let sum = 0
-for (let i = 0; i < str.length;i++){
-(str[i]=='('||str[i]=='['||str[i]=='{')?sum+=1:sum-=1
-if (sum<0) {
-  return false
-  break
-}}
 if (str.length%2!==0) return false
+let sum = 0
+for (let i = 0; i < bracketsConfig.length;i++){
+  for (let j=1; j<str.length;){
+   let open = bracketsConfig[i[0]]
+   let close = bracketsConfig[i[1]]
+   let i1 = 0
+   let i2 = 1
+   let str1 = str.indexOf(open,i1)
+   let str2 = str.indexOf(close,i2)
+   if cur1
+
+
+  }
+}}
 return true
 }
