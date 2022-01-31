cd33e76dbb1ea6c9804ef43a7550f863fe231d3a
AnatoliHaralchuk <sshades@mail.ru>
2022-01-31 15:58:35 +0300

    feat: decided task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = []
+  let close = []
+  stage = []
+  for (let key in bracketsConfig){
+    open.push(bracketsConfig[key][0])
+    close.push(bracketsConfig[key][1])
+  }
+  for (let i = 0; i < str.length; i++){
+    current = str[i]
+    if (open.includes(current)&&close.includes(current)){
+      (stage.includes(current)) ? stage.pop(current) : stage.push(current)
+    }
+    else {if (open.includes(current)){stage.push(current)}
+          else {
+                if (stage.length == 0) return false
+                let top = stage[stage.length - 1]
+                if (close.includes(current)&&(close.indexOf(current) == open.indexOf(top))){
+                  stage.pop(current)
+                }
+              }
+          }
+  }
+  return (stage.length == 0)
 }
